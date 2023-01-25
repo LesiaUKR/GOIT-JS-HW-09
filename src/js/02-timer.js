2b250e5affdae9cf7acfeac32105f3389d6859a2
LesiaUKR <solovyova.lesya@gmail.com>
2023-01-25 18:53:34 +0100

    Timer update

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,34 +1,77 @@
-// import flatpickr from "flatpickr";
-// import "flatpickr/dist/flatpickr.min.css";
-// import Notiflix from 'notiflix';
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
 
-// const startBtn = document.querySelector('button[data-start]');
-// const input = document.querySelector('#datetime-picker');
-// const dataDays = document.querySelector('span[data-days]');
-// const dataHours = document.querySelector('span[data-hours]');
-// const dataMinutes = document.querySelector('span[data-minutes]');
-// const dataSeconds = document.querySelector('span[data-seconds');
-// let timerId = null;
-// startBtn.disabled = true;
+const startBtn = document.querySelector('button[data-start]');
+const input = document.querySelector('#datetime-picker');
+const dataDays = document.querySelector('span[data-days]');
+const dataHours = document.querySelector('span[data-hours]');
+const dataMinutes = document.querySelector('span[data-minutes]');
+const dataSeconds = document.querySelector('span[data-seconds');
 
-// const fp = flatpickr(input);
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     endDate = selectedDates[0];
-//     console.log(selectedDates[0]);
-//     if (endDate <= new Date()) {
-//       console.log(Notiflix.Notify.failure('Please choose a date in the future'));
-//     } else {
-//       startBtn.disabled = false;
-//       console.log(selectedDates[0]);
-//     }
-//   },
-// };
+let timerId = null;
+startBtn.disabled = true;
 
-// flatpickr(input, options);
+const fp = flatpickr(input);
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    endDate = selectedDates[0];
+    console.log(selectedDates[0]);
+    if (endDate <= new Date()) {
+      console.log(Notiflix.Notify.failure('Please choose a date in the future', { position: 'center-top', clickToClose: true }));
+    } else {
+      startBtn.disabled = false;
+      console.log(selectedDates[0]);
+    }
+  },
+};
 
-//   btnStart.addEventListener('click', onStart);
+flatpickr(input, options);
+
+startBtn.addEventListener('click', onStart);
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+}
+  
+function convertMs(ms) {
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+function onStart() {
+    timerId = setInterval(() => {
+
+    let countDown = new Date(input.value) - new Date();
+        if (countDown >= 0) {
+            let time = convertMs(countDown)
+   
+            dataDays.textContent = addLeadingZero(time.days);
+            dataHours.textContent = addLeadingZero(time.hours);
+            dataMinutes.textContent = addLeadingZero(time.minutes);
+            dataSeconds.textContent = addLeadingZero(time.seconds);
+        } else {
+      console.log(Notiflix.Notify.success('CountDown finished',{ position: 'center-top', clickToClose: true }));
+      clearInterval(timerId);
+     }
+       
+  }, 1000)
+}
