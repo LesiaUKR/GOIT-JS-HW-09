1215b3157b8d69063380d1bca3f0935e40a6190a
LesiaUKR <106915140+LesiaUKR@users.noreply.github.com>
2023-01-25 16:38:03 +0000

    Deploying to gh-pages from @ LesiaUKR/GOIT-JS-HW-09_2@fab125eca19bf01cce9c5c2406650edcde9b76b9 🚀

--- 01-color-switcher.f1b9ffaa.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]");console.log(t);const e=document.querySelector("button[data-stop]");console.log(e);const o=document.getElementsByTagName("body")[0];console.dir(o);let n=null;t.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,n=setInterval((()=>{let t=`#${Math.floor(16777215*Math.random()).toString(16)}`;o.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,clearInterval(n)}));
-//# sourceMappingURL=01-color-switcher.f1b9ffaa.js.map
